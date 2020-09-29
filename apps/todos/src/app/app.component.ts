993be65e75d268eec33500bef1f1706526dd63f9
Rene Juuse <rene.juuse@gmail.com>
2020-09-30 00:03:18 +0300

    feat: Display Todos

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Todo } from './models/todo.model';
 
 @Component({
   selector: 'rjd-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'todos';
+  public todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  public addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    });
+  }
 }
